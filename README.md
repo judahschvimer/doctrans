4aebbd8b6c59b54be6004fef22d175688fa82193
Judah Schvimer <judah_schvimer@brown.edu>
2014-06-06 17:02:52 -0400

    more fixing

--- README.md
+++ README.md
@@ -7,3 +7,52 @@ demo: demo used for skunkworksdata used for training
 working: home of translation engine
 
 translate: scripts to generate test data and translations
+
+This may be of help http://www.statmt.org/moses_steps.html
+This also may be of help http://lintaka.wordpress.com/2014/01/17/installing-moses-decoder-on-ubuntu/
+
+Step 1:
+Follow instructions here: http://www.statmt.org/moses/?n=Development.GetStarted
+Install all dependencies as described at the bottom of the document using apt-get if on Ubuntu
+Make sure g++ and Boost are also installed as described in the document
+Clone Moses from Github
+To install it, ensure that Moses uses the correct Boost package
+
+Step 2:
+Install IRSTLM as explained here: http://lintaka.wordpress.com/2014/01/15/installing-irstlm-on-ubuntu/
+In code, make sure that when you call “export IRSTLM=...” you direct it to the proper location
+Install Moses again with IRSTLM
+./bjam --with-irstlm=/usr/local/src/irstlm -j12
+
+Step 3:
+Install MGIZA as described here: http://www.statmt.org/moses/?n=Moses.ExternalTools#ntoc3
+If not already installed, you may need to install cmake for this to work
+Make a tools/ directory in mosesdecoder and put all of the files in the mgiza bin/ directory in it
+also include merge_alignment.py abd sbt2cooc.pl rom the mgiza scripts/ directory
+Finally if it doesn’t work, try to add a file called snt2cooc.out with the following in it:
+${0%/*}/snt2cooc /dev/stdout $1
+$2 $3
+
+Step 4:
+Scrape MongoDB docs for Spanish text
+https://github.com/deafgoat/doctrans/blob/master/extract/pull_test.py
+The above does so, though make sure to run extract_translated_entries() rather than extract_source()
+Move the corpus folder from doctrans to ~/corpus
+You will also need to move the output files from above, doc.en and doc.es, to ~/corpus/training
+
+Step 5:
+Whenever running build_model.sh, make sure directories ~/lm and ~/working do not already exist or build_model.sh will complain
+
+Step 6:
+Make sure when using the “--text” flag (as described in the directions) to NOT say “--text yes” and to only say “--text”
+
+Step 7: 
+When running, run with bash, not just sh
+
+
+
+
+
+
+
+
---
+++ demo/build1.sh
@@ -0,0 +1,66 @@
+#tokenisation
+~/mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < ~/corpus/training/skunkworks.es-en.en > ~/corpus/skunkworks.es-en.tok.en -threads 12
+
+~/mosesdecoder/scripts/tokenizer/tokenizer.perl -l es < ~/corpus/training/skunkworks.es-en.es > ~/corpus/skunkworks.es-en.tok.es -threads 12
+
+# truecasing
+~/mosesdecoder/scripts/recaser/train-truecaser.perl --model ~/corpus/truecase-model.en --corpus ~/corpus/skunkworks.es-en.tok.en
+
+~/mosesdecoder/scripts/recaser/train-truecaser.perl --model ~/corpus/truecase-model.es --corpus ~/corpus/skunkworks.es-en.tok.es
+
+# recasing
+~/mosesdecoder/scripts/recaser/truecase.perl --model ~/corpus/truecase-model.en < ~/corpus/skunkworks.es-en.tok.en > ~/corpus/skunkworks.es-en.true.en
+
+~/mosesdecoder/scripts/recaser/truecase.perl --model ~/corpus/truecase-model.es < ~/corpus/skunkworks.es-en.tok.es > ~/corpus/skunkworks.es-en.true.es
+
+# truncate sentence length to 80 chars
+~/mosesdecoder/scripts/training/clean-corpus-n.perl ~/corpus/skunkworks.es-en.true es en ~/corpus/skunkworks.es-en.clean 1 80
+
+# create lm
+rm -rf ~/lm
+mkdir ~/lm
+cd ~/lm
+~/irstlm/add-start-end.sh < ~/corpus/skunkworks.es-en.true.es > skunkworks.es-en.sb.es
+export IRSTLM=/usr/local/lib/irstlm; ~/irstlm/build-lm.sh -i skunkworks.es-en.sb.es -t ./tmp -p -s improved-kneser-ney -o skunkworks.es-en.lm.es
+
+# create arpa
+~/irstlm/compile-lm --text  skunkworks.es-en.lm.es.gz skunkworks.es-en.arpa.es
+
+# binarize arpa file
+~/mosesdecoder/bin/build_binary skunkworks.es-en.arpa.es skunkworks.es-en.blm.es
+
+echo "Is this a Spanish sentance?" | ~/mosesdecoder/bin/query /home/judah/lm/skunkworks.es-en.blm.es
+
+#######################################
+#  training the translation engine    #
+#                           10:52                                       #
+#######################################
+rm -rf ~/working
+mkdir ~/working
+cd ~/working
+nohup ~/mosesdecoder/scripts/training/train-model.perl -root-dir train -corpus ~/corpus/skunkworks.es-en.clean -f en -e es -alignment grow-diag-final-and -reordering msd-bidirectional-fe -lm 0:3:$HOME/lm/skunkworks.es-en.blm.es:8 -mgiza -external-bin-dir $HOME/mosesdecoder/tools  -cores 12 --parallel >& training.out &
+
+
+######################################
+#  tuning the engine                 #
+######################################
+cd ~/corpus
+~/mosesdecoder/scripts/tokenizer/tokenizer.perl -l es < tuning/news-commentary-v8.es-en.es > news-commentary-v8.es-en.tok.es -threads 12
+~/mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < tuning/news-commentary-v8.es-en.en > news-commentary-v8.es-en.tok.en -threads 12
+
+~/mosesdecoder/scripts/recaser/truecase.perl --model truecase-model.es < news-commentary-v8.es-en.tok.es > news-commentary-v8.es-en.true.es
+~/mosesdecoder/scripts/recaser/truecase.perl --model truecase-model.en < news-commentary-v8.es-en.tok.en > news-commentary-v8.es-en.true.en
+
+cd ~/working
+nohup nice ~/mosesdecoder/scripts/training/mert-moses.pl ~/corpus/news-commentary-v8.es-en.true.en ~/corpus/news-commentary-v8.es-en.true.es ~/mosesdecoder/bin/moses --decoder-flags="-threads 12" train/model/moses.ini ~mertdir ~mosesdeecoder/bin/ &> mert.out &
+
+# binarising the model
+mkdir ~/working/binarised-model
+cd ~/working
+#~/mosesdecoder/bin/processPhraseTable  -ttable 0 0 train/model/phrase-table.gz -nscores 5 -out binarised-model/phrase-table
+#~/mosesdecoder/bin/processLexicalTable -in train/model/reordering-table.wbe-msd-bidirectional-fe.gz -out binarised-model/reordering-table
+#cp ~/working/train/model/moses.ini ~/working/binarised-model
+#sed -i 's/PhraseDictionaryMemory/PhraseDictionaryBinary/' ~/working/binarised-model/moses.ini
+#sed -i 's/train\/model/binarized-model/' ~/working/binarised-model/moses.ini
+
+#~/mosesdecoder/bin/moses -f ~/working/train/model/moses.ini
